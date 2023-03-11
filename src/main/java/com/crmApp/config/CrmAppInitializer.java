6456e0eff2eca0a89ac95fcdf0d859a54b15f57c
Aashutosh <aashutoshsingh126@gmail.com>
2023-03-11 14:40:34 +0530

    Intial Commit issue fixed

--- src/main/java/com/crmApp/config/CrmAppInitializer.java
+++ src/main/java/com/crmApp/config/CrmAppInitializer.java
@@ -7,16 +7,16 @@ import org.springframework.web.WebApplicationInitializer;
 import org.springframework.web.context.support.XmlWebApplicationContext;
 import org.springframework.web.servlet.DispatcherServlet;
 
-public class CrmAppInitializer implements WebApplicationInitializer{
+public class CrmAppInitializer{
 
-    @Override
-    public void onStartup(ServletContext servletContext) throws ServletException {
-        XmlWebApplicationContext appContext = new XmlWebApplicationContext();
-        appContext.setConfigLocation("/WEB-INF/spring/CrmApp-servlet.xml");
-
-        ServletRegistration.Dynamic registration = servletContext.addServlet("dispatcher",
-                new DispatcherServlet(appContext));
-        registration.setLoadOnStartup(1);
-        registration.addMapping("/");
-    }
+//    @Override
+//    public void onStartup(ServletContext servletContext) throws ServletException {
+//        XmlWebApplicationContext appContext = new XmlWebApplicationContext();
+//        appContext.setConfigLocation("/WEB-INF/spring/CrmApp-servlet.xml");
+//
+//        ServletRegistration.Dynamic registration = servletContext.addServlet("dispatcher",
+//                new DispatcherServlet(appContext));
+//        registration.setLoadOnStartup(1);
+//        registration.addMapping("/");
+//    }
 }
--- src/main/java/com/crmApp/controller/TestController.java
+++ src/main/java/com/crmApp/controller/TestController.java
@@ -1,6 +1,7 @@
 package com.crmApp.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
@@ -9,8 +10,8 @@ import org.springframework.web.servlet.ModelAndView;
 @RequestMapping("/test")
 public class TestController {
     @GetMapping
-    public String test(ModelAndView mv){
-        mv.addObject("msg","This is test for CRM App");
+    public String test(ModelMap mp){
+        mp.addAttribute("msg","This is test for CRM App");
         return "test";
     }
 }
