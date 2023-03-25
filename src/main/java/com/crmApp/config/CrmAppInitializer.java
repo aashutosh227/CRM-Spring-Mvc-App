5a7f85e23603e30422bbcb66f3d6fc20b519a53d
Aashutosh <aashutoshsingh126@gmail.com>
2023-03-25 21:08:46 +0530

    configured new dispatcher servlet using Web Application initializer

--- src/main/java/com/crmApp/config/CrmAppInitializer.java
+++ src/main/java/com/crmApp/config/CrmAppInitializer.java
@@ -4,11 +4,35 @@ import jakarta.servlet.ServletContext;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.ServletRegistration;
 import org.springframework.web.WebApplicationInitializer;
+import org.springframework.web.context.WebApplicationContext;
 import org.springframework.web.context.support.XmlWebApplicationContext;
 import org.springframework.web.servlet.DispatcherServlet;
+import org.springframework.web.servlet.support.AbstractDispatcherServletInitializer;
 
-public class CrmAppInitializer{
 
+public class CrmAppInitializer extends AbstractDispatcherServletInitializer{
+
+    @Override
+    protected WebApplicationContext createServletApplicationContext(){
+        XmlWebApplicationContext ctx = new XmlWebApplicationContext();
+        ctx.setConfigLocation("/WEB-INF/spring/CrmApp-servlet.xml");
+        return ctx;
+    }
+
+    @Override
+    protected String[] getServletMappings() {
+        String[] map = {"/crm/*"};
+        return map;
+    }
+
+    @Override
+    protected WebApplicationContext createRootApplicationContext() {
+        return null;
+    }
+}
+//public class CrmAppInitializer extends WebApplicationInitializer{
+
+//
 //    @Override
 //    public void onStartup(ServletContext servletContext) throws ServletException {
 //        XmlWebApplicationContext appContext = new XmlWebApplicationContext();
@@ -19,4 +43,4 @@ public class CrmAppInitializer{
 //        registration.setLoadOnStartup(1);
 //        registration.addMapping("/");
 //    }
-}
+//}
